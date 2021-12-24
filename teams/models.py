0ddb8a81045dcb5bfc8a076b2b43e92845d8fac4
Ronaldo Mata <ronaldomata34@gmail.com>
2021-12-23 22:51:46 -0400

    teams model added

--- teams/models.py
+++ teams/models.py
@@ -1,3 +1,26 @@
 from django.db import models
 
-# Create your models here.
+class Team(models.Model):
+    name = models.CharField(max_length=255)
+    # Emblem
+    emplem_pic = models.ImageField(upload_to='teams/emblems', blank=True, null=True)
+
+    # Coach Detail
+    coach_name = models.CharField(max_length=255, blank=True, null=True)
+    coach_pic = models.ImageField(upload_to='teams/coaches', blank=True, null=True)
+
+    # Shirt
+    shirt_pic = models.ImageField(upload_to='teams/shirts', blank=True, null=True)
+
+    # Stadium
+    stadium = models.ForeignKey('Stadium', on_delete=models.CASCADE, related_name='stadium')
+
+    def __str__(self):
+        return self.name
+
+class Stadium(models.Model):
+    name = models.CharField(max_length=255)
+    pic = models.ImageField(upload_to='teams/stadiums', blank=True, null=True)
+
+    def __str__(self):
+        return self.name
