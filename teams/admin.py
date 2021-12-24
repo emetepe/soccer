a0fc2206e1651fd2bcea7f1e126f613cd1448d28
Ronaldo Mata <ronaldomata34@gmail.com>
2021-12-23 22:52:21 -0400

    teams model added to the admin dashboard

--- teams/admin.py
+++ teams/admin.py
@@ -1,3 +1,22 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Team, Stadium
+
+class TeamAdmin(admin.ModelAdmin):
+    list_display = (
+        'name', 
+        'emblem_pic', 
+        'coach_name', 
+        'coach_pic', 
+        'shirt_pic', 
+        'stadium'
+    )
+
+class StadiumAdmin(admin.ModelAdmin):
+    list_display = (
+        'name', 
+        'pic'
+    )
+
+admin.site.register(Team, TeamAdmin)
+admin.site.register(Stadium, StadiumAdmin)
